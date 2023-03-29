8461156a57b4d9ed0e9c6d7f250f483fa1e5c41b
Aninha <aninhapss2000@gmail.com>
2023-03-28 22:27:23 -0300

    Added autolayout

--- ProjectProtocolZero/ProjectProtocolZero/MyCustomCellXIB.swift
+++ ProjectProtocolZero/ProjectProtocolZero/MyCustomCellXIB.swift
@@ -9,7 +9,7 @@ import UIKit
 import SDWebImage
 
 class MyCustomCellXIB: UITableViewCell {
-
+    
     @IBOutlet var labelPizza: UILabel!
     @IBOutlet var imagePizza: UIImageView!
     
@@ -17,15 +17,16 @@ class MyCustomCellXIB: UITableViewCell {
         super.awakeFromNib()
         // Initialization code
     }
-
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
+        
         // Configure the view for the selected state
     }
     
     func setupXIB(pizza: PizzaElement?) {
         labelPizza.text = pizza?.name
         let urlImage = URL(string: pizza?.imageURL ?? "")
-        imagePizza.sd_setImage(with: urlImage)    }
+        imagePizza.sd_setImage(with: urlImage)
+    }
 }
