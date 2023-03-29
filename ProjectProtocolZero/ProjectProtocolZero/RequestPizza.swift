b3f03429c3a9ce8a959d7e14ff2f62c1c0c6cde4
Aninha <aninhapss2000@gmail.com>
2023-03-28 21:05:01 -0300

    Added Request e closure

--- ProjectProtocolZero/ProjectProtocolZero/RequestPizza.swift
+++ ProjectProtocolZero/ProjectProtocolZero/RequestPizza.swift
@@ -6,7 +6,16 @@
 //
 
 import UIKit
+import Alamofire
 
 class RequestPizza: NSObject {
-
+    
+    func requestPizza(completion: @escaping (Pizza?) -> Void) {
+        AF.request("https://p3teufi0k9.execute-api.us-east-1.amazonaws.com/v1/pizza", method: .get).response { response in
+            
+            let pizza = try? JSONDecoder().decode(Pizza.self, from: response.data ?? Data())
+            
+            completion(pizza)
+        }
+    }
 }
